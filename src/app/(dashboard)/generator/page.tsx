70aa57885eb81995ed0a8b27336f8ee27fef8752
vianismee <104370516+vianismee@users.noreply.github.com>
2025-06-11 23:19:25 +0700

    feat: add Generate Page "BADAN USAHA"

--- src/app/(dashboard)/generator/page.tsx
+++ src/app/(dashboard)/generator/page.tsx
@@ -1,7 +1,27 @@
+import TextGenerator from "@/components/Genrator/TextGenerator";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Bell } from "lucide-react";
+
 export default function GeneratorPage() {
   return (
-    <section>
-      <main>Text Generator</main>
+    <section className="px-5 py-5 flex flex-col gap-5">
+      <Card className="w-full flex justify-center">
+        <CardHeader>
+          <CardTitle className="flex-cols md:flex items-center text-2xl gap-5 justify-between">
+            <div className="inline-flex gap-3 items-center">
+              <Bell height={45} width={45} />
+              <Separator
+                orientation="vertical"
+                className="data-[orientation=vertical]:h-8 data-[orientation=vertical]:w-[3px]"
+              />
+              <h1>Text Generator</h1>
+            </div>
+            <div className="inline-flex gap-3"></div>
+          </CardTitle>
+        </CardHeader>
+      </Card>
+      <TextGenerator />
     </section>
   );
 }
